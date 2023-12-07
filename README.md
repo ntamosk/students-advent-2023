a7e2d089839de71a18f09138658a495d1d375dcc
Your Name <you@example.com>
2023-12-07 14:58:26 +0300

    updates

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # students-advent-2023
 This repository is for students exam practice
+
+## How to use this repository
+1.Clone this repository to your local machine. 
+2.Make changes to the files and push them back to the repository.
+
+```bash
--- accessnumber.sh
+++ accessnumber.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
 read -p "Enter your Reg-No: " regnum
+# Prompting the user to enter the access number, password and email
+read -p "Enter your Access Number: " accessnum
+read -p "Enter your Password: " password
+read -p "Enter your Email: " email
